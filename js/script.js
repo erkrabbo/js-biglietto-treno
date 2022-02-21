5cf1728be2a1b00c153510628aef766ba5b52a05
Luca Andaloro <luca.andaloro4@gmail.com>
2022-02-21 17:39:00 +0100

    BONUS dati con form

--- js/script.js
+++ js/script.js
@@ -1,35 +1,41 @@
-const kmPrice= .21;
-const youngDiscount= .2;
-const oldDiscount= .4;
-let discount;
-
-let km = prompt(`quanti km devi percorrere?`);
-let age = prompt(`quanti anni hai?`);
-
-let ticketCost = km * kmPrice;
-let ticket = document.getElementById("ticket");
-
-
-if (age < 18){
-    discount= youngDiscount;
-	ticketCost = ticketCost - (ticketCost * discount);
-
-    ticket.innerHTML=
-        `Hai ${age} anni per cui è stato applicato uno sconto del ${discount * 100}%.
-        Il prezzo finale del biglietto è €${ticketCost.toFixed(2)}.`;
-
-}else if (age > 65){
-    discount= oldDiscount;
-	ticketCost = ticketCost - (ticketCost * discount);
-
-    ticket.innerHTML=
-        `Hai ${age} anni per cui è stato applicato uno sconto del ${discount * 100}%.
-        Il prezzo finale del biglietto è €${ticketCost.toFixed(2)}.`;
-}
-
-else{
-    ticket.innerHTML=
-        `Il prezzo finale del biglietto è €${ticketCost.toFixed(2)}.`;
-}
-
-console.log(ticketCost)
+let myForm = document.forms.myForm;
+myForm.addEventListener("submit", function (calcolaPrezzo) {
+    const kmPrice= .21;
+    const youngDiscount= .2;
+    const oldDiscount= .4;
+    let discount;
+    
+    let km = document.myForm.km.value;
+        console.log(km);
+    let age = document.myForm.age.value;
+        console.log(age);
+    
+    let ticketCost = km * kmPrice;
+    let ticket = document.getElementById("ticket");
+    
+    
+    if (age < 18){
+        discount= youngDiscount;
+        ticketCost = ticketCost - (ticketCost * discount);
+    
+        ticket.innerHTML=
+            `Hai ${age} anni per cui è stato applicato uno sconto del ${discount * 100}%.
+            Il prezzo finale del biglietto è €${ticketCost.toFixed(2)}.`;
+    
+    }else if (age > 65){
+        discount= oldDiscount;
+        ticketCost = ticketCost - (ticketCost * discount);
+    
+        ticket.innerHTML=
+            `Hai ${age} anni per cui è stato applicato uno sconto del ${discount * 100}%.
+            Il prezzo finale del biglietto è €${ticketCost.toFixed(2)}.`;
+    }
+    
+    else{
+        ticket.innerHTML=
+            `Il prezzo finale del biglietto è €${ticketCost.toFixed(2)}.`;
+    }
+    
+    console.log(ticketCost)
+    calcolaPrezzo.preventDefault();
+})
